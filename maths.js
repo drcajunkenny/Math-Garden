604646d73295315da8f181dbbf63e6cd8632dc4d
KajunKenny <35378531+drcajunkenny@users.noreply.github.com>
2024-02-26 08:25:22 -0700

    update math, images and html

--- maths.js
+++ maths.js
@@ -22,20 +22,20 @@ function checkAnswer() {
     if (prediction == answer) {
         score++;
         console.log(`Correct! Score: ${score}`);
-        if (score <= 6) {
+        if (score <= 7) {
             backgroundImages.push(`url('images/background${score}.svg')`);
             document.body.style.backgroundImage = backgroundImages;
         } else {
-            alert('Well done! Your maths garden is in full bloom! Want to start again?');
+            alert('Well done! Your made your garden full bloom! Want to grow another?');
             score = 0;
             backgroundImages = [];
             document.body.style.backgroundImage = backgroundImages;
         }
     } else {
         if (score != 0) { score--; }
-        console.log(`Wrong. Score ${score}`);
+        console.log(`Try again. Score ${score}`);
 
-        alert('Oops! Check your calculations and try writing the number neater next time!');
+        alert('Oops! Check your calculations or try writing the number neater next time!');
         setTimeout(function () {
             backgroundImages.pop();
             document.body.style.backgroundImage = backgroundImages;
